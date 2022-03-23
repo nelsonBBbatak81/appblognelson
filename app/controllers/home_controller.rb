817f08ef7761dc7ebdc3d00d4392a2cabe635773
nelsonBBbatak81 <butarpondoh@gmail.com>
2022-03-23 17:20:23 +0700

    Make change on send mail again

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,14 +5,23 @@ class HomeController < ApplicationController
 
   def sendMessage
 
-    if params[:name] != '' || params[:email] != '' || params[:message] != ''
-      @message = Contact.new(name: params[:name], email: params[:email], message: params[:message])
-      @message.request = request
-      @message.deliver
+    # if params[:name] != '' || params[:email] != '' || params[:message] != ''
+    #   @message = Contact.new(name: params[:name], email: params[:email], message: params[:message])
+    #   @message.request = request
+    #   @message.deliver
 
-      redirect_to root_path, notice: "Succesfully send message!" 
+    #   redirect_to root_path, notice: "Succesfully send message!" 
+    # else
+    #   redirect_to root_path, notice: "Failed send message!" 
+    # end
+    @contact = Contact.new()
+    @contact.name = params[:name]
+    @contact.email = params[:email]
+    @contact.message = params[:message]
+    if @contact.deliver
+      redirect_to root_path, notice: "Succesfully send message!"
     else
-      redirect_to root_path, notice: "Failed send message!" 
+      redirect_to root_path, notice: "Failed send message! #{@contact.errors}"
     end
 
   end
